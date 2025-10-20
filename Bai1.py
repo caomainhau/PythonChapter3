46eb3aeaf86891dad2103952ba2baa2228f479a6
caomainhau <manhhaucao@gmail.com>
2025-10-20 20:00:15 +0700

    Initial commit

---
+++ Bai1.py
@@ -0,0 +1,10 @@
+filename = '/Users/manhhaucao/alice.txt'
+try:
+    with open(filename) as f_obj:
+         contents = f_obj.read()
+except FileNotFoundError:
+     msg = "File " + filename + " không tồn tại."
+     print(msg)
+else:
+     msg = "File " + filename + " tồn tại."
+     print(msg)
